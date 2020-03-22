880483743356765d2db558e3963fd3b8eee1fdcc
Yujian <yujian_fu@outlook.com>
2020-03-23 03:29:53 +0800

    first version

--- graph_construction/buildkNN.cpp
+++ graph_construction/buildkNN.cpp
@@ -11,10 +11,11 @@ using namespace std;
 int main()
 {
     //test the get_proportion_dataset
-    string str = "/home/yujian/Downloads/similarity_search_datasets/ANN_SIFT10K/siftsmall_base.fvecs";
+    //string str = "/home/yujian/Downloads/similarity_search_datasets/ANN_SIFT10K/siftsmall_base.fvecs";
+    string str = "/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1M/sift_base.fvecs";
     char* filename = const_cast<char*>(str.c_str());
     clock_t start,finish;
-    /*
+    
     start = clock();
     merge_kNN(filename);
     finish = clock();
@@ -24,7 +25,7 @@ int main()
     transitive_reduction(filename);
     finish = clock();
     cout << "Transitive Reduction takes " << (double)(finish - start) / CLOCKS_PER_SEC << " second." << endl;
-    */
+    
     start = clock();
     search(filename);
     finish = clock();
