431f8722878a2bf97fa49f403a29df665e017933
Yujian <yujian_fu@outlook.com>
2020-03-27 15:52:21 +0800

    billion scale

--- billion_scale_search/buildkNN.py
+++ billion_scale_search/buildkNN.py
@@ -9,6 +9,7 @@ x = np.memmap(filename, dtype='uint8', mode='r')
 d = x[:4].view('int32')[0]
 x = x.reshape(-1, d + 4)[:, 4:]
 dataset_size = x.shape[0]
+print("The dataset size is ", dataset_size)
 k = 100
 niter = 100
 proportion = 0.01
@@ -19,7 +20,7 @@ for i in range(int(1/proportion)):
     print(i, " th iteration")
     #processing_subset = np.ascontiguousarray(x[int(dataset_size*proportion*i) : int(dataset_size*proportion*(i+1)), :].astype('float32'))
     processing_subset = np.ascontiguousarray(x[int(dataset_size*proportion*i) : int(dataset_size*proportion*(i+1)), :])
-    print("Finish data loading, start clusteringw")
+    print("Finish data loading, start clustering")
     #kmeans = faiss.Kmeans(d, k, niter = niter, verbose = True, gpu=True)
     #kmeans.train(processing_subset)
     #print(kmeans.obj)
