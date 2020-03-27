fe4c3c925d2d0942f8c881b9e1532a9c08829d3d
Yujian <yujian_fu@outlook.com>
2020-03-27 15:39:48 +0800

    billion scale

--- billion_scale_search/buildkNN.py
+++ billion_scale_search/buildkNN.py
@@ -1,6 +1,8 @@
 import faiss
 import numpy as np 
 import random
+from sklearn.cluster import KMeans
+
 
 filename = "/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1B/1milliard.p1.siftbin"
 x = np.memmap(filename, dtype='uint8', mode='r')
@@ -14,10 +16,16 @@ proportion = 0.01
 
 
 for i in range(int(1/proportion)):
-    processing_subset = np.ascontiguousarray(x[int(dataset_size*proportion*i) : int(dataset_size*proportion*(i+1)), :].astype('float32'))
-    kmeans = faiss.Kmeans(d, k, niter = niter, verbose = True, gpu=True)
-    kmeans.train(processing_subset)
-    print(kmeans.obj)
-    print(kmeans.centroids)
-
+    print(i, " th iteration")
+    #processing_subset = np.ascontiguousarray(x[int(dataset_size*proportion*i) : int(dataset_size*proportion*(i+1)), :].astype('float32'))
+    processing_subset = np.ascontiguousarray(x[int(dataset_size*proportion*i) : int(dataset_size*proportion*(i+1)), :])
+    #kmeans = faiss.Kmeans(d, k, niter = niter, verbose = True, gpu=True)
+    #kmeans.train(processing_subset)
+    #print(kmeans.obj)
+    #print(kmeans.centroids)
+    kmeans = KMeans(n_clusters=100, n_init=100, max_iter=300,
+    verbose=True, n_jobs=-1).fit(processing_subset)
+    print(kmeans.labels_)
+    print(kmeans.cluster_centers_)
+    
 
