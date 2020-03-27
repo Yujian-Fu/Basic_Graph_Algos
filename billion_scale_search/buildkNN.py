54a113626058cceaa9709e6a1b32cf6db8851a30
Yujian <yujian_fu@outlook.com>
2020-03-27 14:05:23 +0800

    billion scale

--- billion_scale_search/buildkNN.py
+++ billion_scale_search/buildkNN.py
@@ -1,78 +1,23 @@
 import faiss
 import numpy as np 
 import random
-import time
 
 filename = "/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1B/1milliard.p1.siftbin"
 x = np.memmap(filename, dtype='uint8', mode='r')
 d = x[:4].view('int32')[0]
 x = x.reshape(-1, d + 4)[:, 4:]
+dataset_size = x.shape[0]
+k = 100
+niter = 100
+proportion = 0.1
+#index = random.sample(range(0, x.shape[0]), int(proportion * x.shape[0]))
 
-print("The shape of x is ", x.shape)
-print("testing random read and sequential read")
+
+for i in range(int(1/proportion)):
+    processing_subset = np.ascontiguousarray(x[dataset_size*proportion*i, dataset_size*proportion*(i+1), :])
+    kmeans = faiss.Kmeans(d, k, niter = niter, verbose = True)
+    kmeans.train(processing_subset)
+    print(kmeans.obj)
+    print(kmeans.centroids)
 
 
-print("1000 instances")
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 1000)
-dataset = np.ascontiguousarray(x[index, :])
-endtime = time.time()
-print (endtime - starttime)
-
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 1000)
-dataset = np.ascontiguousarray(x[0:1000, :])
-endtime = time.time()
-print (endtime - starttime)
-
-print("10000 instances")
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 10000)
-dataset = np.ascontiguousarray(x[index, :])
-endtime = time.time()
-print (endtime - starttime)
-
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 10000)
-dataset = np.ascontiguousarray(x[0:10000, :])
-endtime = time.time()
-print (endtime - starttime)
-
-print("100000 instances")
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 10000)
-dataset = np.ascontiguousarray(x[index, :])
-endtime = time.time()
-print (endtime - starttime)
-
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 10000)
-dataset = np.ascontiguousarray(x[0:10000, :])
-endtime = time.time()
-print (endtime - starttime)
-
-print("1000000 instances")
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 1000000)
-dataset = np.ascontiguousarray(x[index, :])
-endtime = time.time()
-print (endtime - starttime)
-
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 1000000)
-dataset = np.ascontiguousarray(x[0:1000000, :])
-endtime = time.time()
-print (endtime - starttime)
-
-print("10000000 instances")
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 10000000)
-dataset = np.ascontiguousarray(x[index, :])
-endtime = time.time()
-print (endtime - starttime)
-
-starttime = time.time()
-index = random.sample(range(0, x.shape[0]), 10000000)
-dataset = np.ascontiguousarray(x[0:10000000, :])
-endtime = time.time()
-print (endtime - starttime)
---
+++ billion_scale_search/test_random_sequential.py
@@ -0,0 +1,78 @@
+import faiss
+import numpy as np 
+import random
+import time
+
+filename = "/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1B/1milliard.p1.siftbin"
+x = np.memmap(filename, dtype='uint8', mode='r')
+d = x[:4].view('int32')[0]
+x = x.reshape(-1, d + 4)[:, 4:]
+
+print("The shape of x is ", x.shape)
+print("testing random read and sequential read")
+
+
+print("1000 instances")
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 1000)
+dataset = np.ascontiguousarray(x[index, :])
+endtime = time.time()
+print (endtime - starttime)
+
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 1000)
+dataset = np.ascontiguousarray(x[0:1000, :])
+endtime = time.time()
+print (endtime - starttime)
+
+print("10000 instances")
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 10000)
+dataset = np.ascontiguousarray(x[index, :])
+endtime = time.time()
+print (endtime - starttime)
+
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 10000)
+dataset = np.ascontiguousarray(x[0:10000, :])
+endtime = time.time()
+print (endtime - starttime)
+
+print("100000 instances")
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 100000)
+dataset = np.ascontiguousarray(x[index, :])
+endtime = time.time()
+print (endtime - starttime)
+
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 100000)
+dataset = np.ascontiguousarray(x[0:10000, :])
+endtime = time.time()
+print (endtime - starttime)
+
+print("1000000 instances")
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 1000000)
+dataset = np.ascontiguousarray(x[index, :])
+endtime = time.time()
+print (endtime - starttime)
+
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 1000000)
+dataset = np.ascontiguousarray(x[0:1000000, :])
+endtime = time.time()
+print (endtime - starttime)
+
+print("10000000 instances")
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 10000000)
+dataset = np.ascontiguousarray(x[index, :])
+endtime = time.time()
+print (endtime - starttime)
+
+starttime = time.time()
+index = random.sample(range(0, x.shape[0]), 10000000)
+dataset = np.ascontiguousarray(x[0:10000000, :])
+endtime = time.time()
+print (endtime - starttime)
